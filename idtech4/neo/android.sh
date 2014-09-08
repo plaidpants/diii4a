afac7a5c81750c6222003357b78166cd140774ee
Marcelo Martins <martins@cs.brown.edu>
2014-09-08 19:11:21 -0400

    Make it compile with gcc-4.8

--- idtech4/neo/android.sh
+++ idtech4/neo/android.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-export PATH=/home/martins/android-ndk-r9d/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86_64/bin:$PATH
+export PATH=/home/martins/android-ndk-r9d/toolchains/arm-linux-androideabi-4.8/prebuilt/linux-x86_64/bin:$PATH
 export ARCH=arm-linux-androideabi
 export CXX=$ARCH-g++
 export CC=$ARCH-gcc
-export SYSROOT=/opt/android-toolchain/sysroot
+export NDK_TOOLCHAIN_VERSION=4.8
 
 scons \
 	ARCH='arm' \
@@ -17,7 +17,5 @@ scons \
 	NOCURL=1 \
 	TARGET_ANDROID=1 \
 	TARGET_D3XP=0 \
-	RPATH="${SYSROOT}/usr/lib"
-	LIBPATH="/home/martins/android-ndk-r9d/sources/cxx-stl/gnu-libstdc++/4.6/libs/armeabi"
 	BASEFLAGS="-march=armv7-a -fno-builtin-sin -fno-builtin-cos -fno-builtin-sinf -fno-builtin-cosf -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp -Wl,--no-undefined -fexceptions -frtti"\
 	$*
